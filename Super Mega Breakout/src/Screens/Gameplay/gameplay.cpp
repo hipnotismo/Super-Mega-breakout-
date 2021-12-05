973839fe76b0d4ad2d87fe4760c1b33608b66c8b
martin <martinconcetti97@gmail.com>
2021-12-05 02:25:54 -0300

    Player init integrated into gameplay
    And the player now drawn in the middle of the screen

--- Super Mega Breakout/src/Screens/Gameplay/gameplay.cpp
+++ Super Mega Breakout/src/Screens/Gameplay/gameplay.cpp
@@ -9,18 +9,21 @@ namespace gameplay {
 	Player player;
 
 	void gameplayInit() {
-		player.pos = { static_cast<float>(GetScreenWidth())/10,static_cast<float>(GetScreenHeight())/10};
-		player.size = { static_cast<float>(GetScreenWidth()) / 10,static_cast<float>(GetScreenHeight()) / 10 };
+		player.size = { static_cast<float>(GetScreenWidth()) / 6,static_cast<float>(GetScreenHeight()) / 15 };
+		player.pos = { static_cast<float>(GetScreenWidth()) / 2 - player.size.x/2,static_cast<float>(GetScreenHeight()) - player.size.y - 10 };
+		std::cout<< player.size.x / 2 << std::endl;
 	}
 
 	void gameplayUpdate() {
 		gameplayDraw();
+
+
 	}
 
 	void gameplayDraw() {
 		ClearBackground(RAYWHITE);
 
-		DrawRectangle(static_cast<int>(player.pos.x), static_cast<int>(player.pos.y), static_cast<int>(player.size.y), static_cast<int>(player.size.x), BLACK);
+		DrawRectangle(static_cast<int>(player.pos.x), static_cast<int>(player.pos.y), static_cast<int>(player.size.x), static_cast<int>(player.size.y), BLACK);
 
 		EndDrawing();
 	}
